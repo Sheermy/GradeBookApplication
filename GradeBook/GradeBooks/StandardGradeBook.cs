7a15819341ab03802747fe3ec1ec06fc37e3785c
Sheermy <dopa9669@gmail.com>
2026-03-05 09:52:45 +0100

    Dodanie StandardGradeBook

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,13 @@
+﻿using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        // Konstruktor przyjmuje tylko nazwę, ale do bazy przekazuje nazwę i typ Standard
+        public StandardGradeBook(string name) : base(name, GradeBookType.Standard)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
