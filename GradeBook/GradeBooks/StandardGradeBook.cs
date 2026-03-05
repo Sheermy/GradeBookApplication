6839744b5beeb8f8de250dbaeb9f90ffc9d07905
Sheermy <dopa9669@gmail.com>
2026-03-05 09:58:05 +0100

    RankedGradeBooks i logika oceniania

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -4,7 +4,6 @@ namespace GradeBook.GradeBooks
 {
     public class StandardGradeBook : BaseGradeBook
     {
-        // Konstruktor przyjmuje tylko nazwę, ale do bazy przekazuje nazwę i typ Standard
         public StandardGradeBook(string name) : base(name, GradeBookType.Standard)
         {
             Type = GradeBookType.Standard;
